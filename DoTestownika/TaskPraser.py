feedb1b1d870020805100744fe35f3ba44174b22
Górka Mateusz <maatiug@gmail.com>
2021-01-15 19:46:01 +0100

    added TaskPraser

--- DoTestownika/TaskPraser.py
+++ DoTestownika/TaskPraser.py
@@ -5,4 +5,36 @@ class TaskPraser:
     @staticmethod
     def prase(text: str) -> str:
         """ Prase task text to testownik file """
-        raise NotImplementedError()
+        task = TaskPraser(text)
+        return task.generate()
+
+    def __init__(self, text_input):
+        self.input = text_input.split("\n")
+        self.true = None
+        self.parse_answers()
+
+    def generate(self) -> str:
+        """ private - Generate testownik file """
+        return f"{self.get_ans_code()}\n{self.get_content()}{self.get_ans_stat()}\nPRAWDA\nFAŁSZ"
+
+    def get_ans_code(self) -> str:
+        """ private """
+        if self.true is None:
+            return "X11"
+        else:
+            return "X10" if self.true else "X01"
+
+    def get_ans_stat(self) -> str:
+        """ private """
+        return ""
+        # TODO s answer statictis
+
+    def get_content(self):
+        """ private """
+        return "\n".join(self.input[0:-1])
+
+    def parse_answers(self):
+        """ private """
+        text = self.input[-1].split(" | ")
+        if "? ? ?" not in text[0]:
+            self.true = "PRAWDA" in text[0]
