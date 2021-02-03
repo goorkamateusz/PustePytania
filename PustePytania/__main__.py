a133ae9e27c147b830fbb562e9572e97f6715781
Górka Mateusz <maatiug@gmail.com>
2021-02-03 18:45:59 +0100

    small docs changes

--- PustePytania/__main__.py
+++ PustePytania/__main__.py
@@ -15,34 +15,45 @@ if __name__ == "__main__":
 
     @bot.event
     async def on_ready():
-        """ Komunikat o poprawnym połaczeniu """
+        """
+        Wyświetla komunikat o poprawnym połaczeniu
+        """
         for guild in bot.guilds:
             print( f'{bot.user} jest połączony z:\n{guild.name}(id: {guild.id})\n' )
 
 
     @bot.command(name="readchannel")
     async def readchannel(ctx):
-        """ Czyta i przetwarza historie z kanału, na którym został wywołany. """
+        """
+        Czyta i przetwarza obrazy z historii z kanału, na którym został wywołany.
+        """
         await PustePytania.readchannel( ctx, Config.file_head )
         RenameFiles.rename(Config.EXAM_NAME)
 
 
     @bot.command(name="readlast")
     async def readlast(ctx):
-        """ Czyta i przetwarza wiadomości do najświeższej reakcji 🆕"""
+        """
+        Czyta i przetwarza obrazy z historii do najświeższej reakcji 🆕 (tylko ostatni tesst)
+        """
         await PustePytania.readchannel( ctx, Config.file_head, 1 )
         RenameFiles.rename(Config.EXAM_NAME)
 
 
     @bot.command(name="download")
     async def download(ctx, arg):
-        """ Pobiera pliki o określonym formacie z kanału """
+        """ Pobiera pliki o określonym formacie z kanału. """
         await Downloader.get_all_from_channel(ctx, arg)
 
 
     @bot.command(name="echo")
     async def send_message(ctx, arg):
-        """ Wysyła wiadomość o podanej treści wiadomość niżej """
+        """
+        Wysyła wiadomość o podanej treści wiadomość niżej.
+
+        Na przykład:
+            !echo "treść do wyświetlenia"
+        """
         await Report.echo(ctx, arg)
 
 
